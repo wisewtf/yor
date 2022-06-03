fb886ab0665ebbbe79d2267afc39d2c44dcfa756
wisewtf <wise@wise.wtf>
2022-06-03 17:09:37 +0200

    fixed OS env var

--- bot_start.py
+++ bot_start.py
@@ -1,10 +1,13 @@
-import urllib, json, interactions, re, requests, helpers
+import json, interactions, re, requests, helpers
 
-def get_data():
+def cotw():
     r =requests.get(helpers.FIRSTS_LOCG_URL)
     firsts = str(json.loads(r.text))
     match_firsts = re.findall("\/comic\/.*?-1", firsts)
-    return  match_firsts
+    body = ""
+    for match in match_firsts:
+        body=body+"\n https://leagueofcomicgeeks.com"+match
+    return  body
 
 bot = interactions.Client(token=helpers.BOT_TOKEN())
 
@@ -12,14 +15,10 @@ bot = interactions.Client(token=helpers.BOT_TOKEN())
     name="cotw",
     description="Replies with this week's #1 comic books on LOCG",
     # scope is optional. So you can use it or not, in case you do remember to set the env variable
-    scope=helpers.GUILD_SCOPE(),
+    scope=helpers.GUILD_SCOPE,
 )
 
 async def only_firsts(ctx: interactions.CommandContext):
-    data = get_data()
-    body = ""
-    for match in data:
-        body=body+"\n https://leagueofcomicgeeks.com"+match
-    await ctx.send(body)
+    await ctx.send(cotw())
 
 bot.start()
--- helpers.py
+++ helpers.py
@@ -5,7 +5,6 @@ def BOT_TOKEN():
 
 
 # This one is only needed if you use the scope option in the main script
-def GUILD_SCOPE():
-    return os.environ['GUILD_SCOPE']
+GUILD_SCOPE = int(os.environ['GUILD_SCOPE'])
 
 FIRSTS_LOCG_URL = "https://leagueofcomicgeeks.com/comic/get_comics?addons=1&list=releases&publisher[]=2&publisher[]=7&publisher[]=1&publisher[]=5&publisher[]=13&list_refinement=firsts&format%5B%5D=1&date_type=week&date="
